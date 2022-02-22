969ccb45faac457d22efdd055ab3ccaea1d4e5b1
jayakrishnan cn <jayakrishnancn@gmail.com>
2022-02-22 20:45:39 +0530

    bugfix

--- lib/repository/account_repo.dart
+++ lib/repository/account_repo.dart
@@ -2,6 +2,7 @@ import 'package:budget/models/account.dart';
 
 abstract class AccountRepo {
   Future<List<Account>> getAccounts();
+  Future<Account> getAccount(String id);
   Future<void> createAccount(Account account);
   Future<void> updateAccount(Account account);
   Future<void> deleteAccount(String id);
--- lib/repository/firebase/account_repo.dart
+++ lib/repository/firebase/account_repo.dart
@@ -1,37 +1,19 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import '../../exceptions/service_expception.dart';
 import '../../models/account.dart';
 import '../account_repo.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 
 class AccountFirebaseRepo extends AccountRepo {
-  String userid = 'jayakrishnancn@gmail.com';
+  final String userid = 'jayakrishnancn@gmail.com';
 
-  @override
-  Future<void> createAccount(Account account) {
-    return FirebaseFirestore.instance
-        .collection('users/$userid/accounts')
-        .add(account.toJSON());
-  }
-
-  @override
-  Future<void> updateAccount(Account account) {
-    var docRef = FirebaseFirestore.instance
-        .collection('users/$userid/accounts')
-        .doc(account.id);
-    return docRef.get().then((doc) {
-      if (doc.exists) {
-        return docRef.set(account.toJSON());
-      }
-      return Future.error('Account does not exist.');
-    });
-  }
+  CollectionReference<Map<String, dynamic>> get root =>
+      FirebaseFirestore.instance.collection('users/$userid/accounts');
 
   @override
   Future<List<Account>> getAccounts() async {
     List<Account> accounts = [];
 
-    final querySnapshot = await FirebaseFirestore.instance
-        .collection('users/$userid/accounts')
-        .get();
+    final querySnapshot = await root.get();
     for (var map in querySnapshot.docs) {
       accounts.add(Account.fromSnap(map.data(), docId: map.id));
     }
@@ -40,11 +22,36 @@ class AccountFirebaseRepo extends AccountRepo {
   }
 
   @override
+  Future<Account> getAccount(String id) {
+    return root.doc(id).get().then((value) {
+      if (value.exists) {
+        Account.fromSnap(value.data()!, docId: value.id);
+      }
+      throw ServiceException('can\'t find the account');
+    });
+  }
+
+  @override
+  Future<void> createAccount(Account account) {
+    return root.add(account.toJSON());
+  }
+
+  @override
+  Future<void> updateAccount(Account account) {
+    var docRef = root.doc(account.id);
+
+    return docRef.get().then((doc) {
+      if (doc.exists) {
+        return docRef.set(account.toJSON());
+      }
+      return Future.error('Account does not exist.');
+    }).catchError((error) {
+      throw ServiceException('Cant find account');
+    });
+  }
+
+  @override
   Future<void> deleteAccount(String id) {
-    print(id);
-    return FirebaseFirestore.instance
-        .collection('users/$userid/accounts')
-        .doc(id)
-        .delete();
+    return root.doc(id).delete();
   }
 }
